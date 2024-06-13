bc3b0e2b3950bfd80941d2cd6aef4eb958ee6ac0
Hanna Gawlitza <hannagawlitza@Hannas-MacBook-Pro.local>
2024-06-13 12:51:18 +0200

    kmom03 - set up a GameController amd routes game/home, game/doc.

---
+++ src/Controller/GameController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Card\CardDeck;
+use App\Card\Card;
+use App\Card\CardGraphic;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class GameController extends AbstractController
+{
+    #[Route("/game", name: "game")]
+    public function gameHome(): Response
+    {
+        return $this->render('game/home.html.twig');
+    }
+
+    #[Route("/card/doc", name: "doc")]
+    public function gameDoc(): Response
+    {
+        return $this->render('game/doc.html.twig');
+    }
+}
