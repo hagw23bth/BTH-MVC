1f35a8d95c007a7830bccad5c1075222451b5944
Hanna Gawlitza <hannagawlitza@Hannas-MacBook-Pro.local>
2024-06-20 14:05:15 +0200

    kmom04 - cardTest

---
+++ tests/Card/CardTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Tests\Card;
+
+use PHPUnit\Framework\TestCase;
+use App\Card\Card;
+
+class CardTest extends TestCase
+{
+    // Kontrollera att getSuit() ger förväntad retur
+    public function testGetSuit()
+    {
+        $card = new Card('Hearts', 10);
+        $this->assertEquals('Hearts', $card->getSuit());
+    }
+
+    // Kontrollerar att getValue() ger förväntad retur
+    public function testGetValue()
+    {
+        $card = new Card('Spades', 5);
+        $this->assertEquals(5, $card->getValue());
+    }
+
+    // Kontrollerar att toString() ger förväntad retur
+    public function testToString()
+    {
+        $card = new Card('Diamonds', 10);
+        $this->assertEquals('10 of Diamonds', (string) $card);
+    }
+
+	// Kontrollera att toArray() returnerar en array med korrekta suit- och value-värden.
+    public function testToArray()
+    {
+        $card = new Card('Clubs', 8);
+        $expectedArray = [
+            'suit' => 'Clubs',
+            'value' => 8
+        ];
+        $this->assertEquals($expectedArray, $card->toArray());
+    }
+}
