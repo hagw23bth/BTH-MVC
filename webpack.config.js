ef8fcc6be3609fdbf44eea10a352097b0dceb561
Hanna Gawlitza <hagw23@student.bth.se>
2024-08-20 14:11:28 +0200

    kmom10 - basic style, home page, nav and header

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,9 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    
+    // Entry for the proj page
+    .addEntry('proj', './assets/proj.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
